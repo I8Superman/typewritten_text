80546f806c0647b41375ac2ccafdf8c46bb84b36
Simon Andersen <simo414u@stud.kea.dk>
2021-02-09 18:57:29 +0100

    Everthing works, but trying to make function that works on multiple elements in a row

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,32 @@
 "use strict";
-"JS is running"
 
-const qs = (s) => document.querySelector(s);
-const typeText = qs('.typewriten');
+// Global variables
+const qs = (s) => document.querySelector(s); // General querySelector abbriviation
+⁄
+let letterN = 0; // The N'th number of letter in texttoType
+const typeElem = qs('#typewriter'); // Element to add the text to
+const extraElem = qs('#extrainfo'); // Extra element to add the text to
+const textLength = texttoType.length; // Measuring the number of charecters in the texttoType
 
-console.log('typeText');
+function init() {
+    qs('.typewritten').textContent = ""; // Remove the text from the DOM
+    typeText(typeElem);
+}
+
+function typeText(elem) {
+    elem.textContent = texttoType.substring(0, letterN); // Increase letters in string and print
+    letterN++;// Increment N
+    const randomTypeSpeed = Math.floor(Math.random() * 6) + 1;// Setting realistic variable typing speed!
+    // Check is all letters have been printed
+    if (letterN != textLength + 1) {
+        setTimeout(typeText, randomTypeSpeed * 50);
+    } else {
+        endThings();
+    }
+}
+
+function endThings() {
+
+}
+
+window.addEventListener('DOMContentLoaded', init());
