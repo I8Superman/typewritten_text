5857efa0413c8659c2e9349a61288a5df78d38ae
Simon Andersen <simo414u@stud.kea.dk>
2021-02-10 15:10:37 +0100

    Finished. Now works for any number of elements

--- typewriter.js
+++ typewriter.js
@@ -1,48 +1,38 @@
 "use strict";
-
-
 // Global variables
 const qs = (s) => document.querySelector(s); // General querySelector abbriviation
 
-let texttoType;// Store orignal elem text in this variable
+let texttoType = [];// Store orignal text from elem0 in this variable
 let letterN = 1; // The N'th number of letter in the texttoType (see typeText function below)
 const getElems = document.querySelectorAll('.typewritten'); // Get all .typewritten elems as an array
 let arrayNr = 0;
-const typeElem = getElems[arrayNr]; // Get the first element from the array
 
-function init() {
-    getElems.forEach(typeText);
+function getText() {
+    getElems.forEach(elem => { // Stores txt from all elems as an array in the texttoType var
+        texttoType.push(elem.textContent);
+    });
+    getElems.forEach(elem => elem.textContent = "");// Removes text content from all the elements
+    typeText();
 }
 
-function typeText(typeelem) {
-    console.log(typeelem);
-    texttoType = typeelem.textContent;
-    typeelem.textContent = "";
-    console.log(texttoType);
-    // typeLoop();
-}
+function typeText() {
+    getElems[arrayNr].textContent = texttoType[arrayNr].substring(0, letterN); // Increase letters in string and print the string
+    const textLength = texttoType[arrayNr].length; // getting the text length of the current string object
 
-function typeLoop() {
-    console.log('typeLoop');
-    const textLength = texttoType.length; // Measuring the number of charecters in the texttoType
-    typeElem.textContent = texttoType.substring(0, letterN); // Increase letters in string and print the string
-    console.log(typeElem.textContent);
-    //console.log(letterN);
-    if (letterN != textLength) { // Check the number of letters printed is not yet the full amount
+    if (letterN <= textLength) { // Check the number of letters printed is not yet the full amount
         letterN++; // Increment N -> the number of letters to be printed next time 
-        //console.log(letterN);
         const randomTypeSpeed = Math.floor(Math.random() * 6) + 1;// Setting realistic variable typing speed!
-        setTimeout(typeLoop, randomTypeSpeed * 50);
+        setTimeout(typeText, randomTypeSpeed * 50);
     } else {
-        letterN = 1;
-        typeElem.classList.remove('.typewritten');
-        typeElem.classList.add('.donetyping');
-        endThings();
+        letterN = 1; // Reset letterN for next string to be printed
+        arrayNr++; // Increment arrayNr to select next string in texttoType array for printing
+        console.log(letterN, arrayNr, texttoType.length);
+        if (arrayNr >= texttoType.length) { // If arrayNr 
+            console.log('No more elements to type!');
+        } else {
+            typeText(); // Run function again - now selecting the next string in the texttoType array
+        }
     }
 }
 
-function endThings() {
-
-}
-
-window.addEventListener('DOMContentLoaded', init());
+window.addEventListener('DOMContentLoaded', getText());
